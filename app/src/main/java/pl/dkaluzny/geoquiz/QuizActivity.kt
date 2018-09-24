01e82ce64f4a6caa24578a4399a8ff484c3bec9b
kalug89 <kaluzny_david@hotmail.com>
2018-09-24 22:32:59 +0200

    Done project GeoQuiz

--- app/src/main/java/pl/dkaluzny/geoquiz/QuizActivity.kt
+++ app/src/main/java/pl/dkaluzny/geoquiz/QuizActivity.kt
@@ -2,11 +2,21 @@ package pl.dkaluzny.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        true_button.setOnClickListener{
+            Toast.makeText(this@QuizActivity, R.string.true_button, Toast.LENGTH_LONG).show()
+        }
+        false_button.setOnClickListener {
+            Toast.makeText(this@QuizActivity, R.string.false_button, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
